7b66439e41f16b7004a2ba5318e06723ea017041
Karmalized <Matthew.H.Tran-1@ou.edu>
2025-04-03 01:12:49 -0500

    reworked implementation

--- src/App.js
+++ src/App.js
@@ -47,25 +47,25 @@ function App() {
               <div className='Upper'>
               <div>
               <h4>Proximity Based Models</h4>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_1.png'} className='plot'/>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_2.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_1.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_2.png'} className='plot'/>
               </div>
               <div>
               <h4>Probabilistic Based Models</h4>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_3.png'} className='plot'/>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_4.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_3.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_4.png'} className='plot'/>
               </div>
               </div>
               <div className='Lower'>
               <div>
               <h4>Ensemble Encoding Models</h4>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_5.png'} className='plot'/>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_6.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_5.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_6.png'} className='plot'/>
               </div>
               <div>
               <h4>Linear Models</h4>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_7.png'} className='plot'/>
-              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-03-31 00-17-48_8.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_7.png'} className='plot'/>
+              <img src={'/hw-3-photos/wholesale_dataset/plot_2025-04-03 01-05-17_8.png'} className='plot'/>
               </div>
               </div>
             </div>
@@ -80,25 +80,25 @@ function App() {
               <div className='Upper'>
               <div>
               <h4>Proximity Based Models</h4>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_1.png'} className='plot'/>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_2.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_1.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_2.png'} className='plot'/>
               </div>
               <div>
               <h4>Probabilistic Based Models</h4>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_3.png'} className='plot'/>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_4.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_3.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_4.png'} className='plot'/>
               </div>
               </div>
               <div className='Lower'>
               <div>
               <h4>Ensemble Encoding Models</h4>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_5.png'} className='plot'/>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_6.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_5.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_6.png'} className='plot'/>
               </div>
               <div>
               <h4>Linear Models</h4>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_7.png'} className='plot'/>
-              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-03-31 01-03-09_8.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_7.png'} className='plot'/>
+              <img src={'/hw-3-photos/breast_cancer_dataset/plot_2025-04-03 01-02-13_8.png'} className='plot'/>
               </div>
               </div>
             </div>
@@ -113,25 +113,25 @@ function App() {
               <div className='Upper'>
               <div>
               <h4>Proximity Based Models</h4>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_1.png'} className='plot'/>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_2.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_1.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_2.png'} className='plot'/>
               </div>
               <div>
               <h4>Probabilistic Based Models</h4>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_3.png'} className='plot'/>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_4.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_3.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_4.png'} className='plot'/>
               </div>
               </div>
               <div className='Lower'>
               <div>
               <h4>Ensemble Encoding Models</h4>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_5.png'} className='plot'/>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_6.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_5.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_6.png'} className='plot'/>
               </div>
               <div>
               <h4>Linear Models</h4>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_7.png'} className='plot'/>
-              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-02 00-03-22_8.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_7.png'} className='plot'/>
+              <img src={'/hw-3-photos/auto_mpg_dataset/plot_2025-04-03 01-04-54_8.png'} className='plot'/>
               </div>
               </div>
             </div>
